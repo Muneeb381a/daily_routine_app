c8f8eb083255418c39584e980bb577bf408a536b
Muneeb381a <muneebafzal381a@gmail.com>
2025-05-22 13:55:54 +0500

    Simplify database connection configuration by using DATABASE_URL and removing individual environment variable settings

--- src/config/db.js
+++ src/config/db.js
@@ -20,14 +20,8 @@ for (const varName of requiredVarNames) {
 }
 
 const pool = new Pool({
-  port: parseInt(process.env.DB_PORT, 10),
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  max: 10,
-  idleTimeoutMillis: 3000,
-  connectionTimeoutMillis: 2000,
+ connectionString: process.env.DATABASE_URL,
+ ssl: {rejectUnauthorized: false}
 });
 
 export { pool };
