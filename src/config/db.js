d0cd92011cce4988556d1607fa1f2dd2da5a2094
Muneeb381a <muneebafzal381a@gmail.com>
2025-05-22 13:47:30 +0500

    Refactor database configuration for improved readability and consistency

--- src/config/db.js
+++ src/config/db.js
@@ -3,28 +3,31 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const {Pool} = pkg;
+const { Pool } = pkg;
 
+const requiredVarNames = [
+  "DB_PORT",
+  "DB_HOST",
+  "DB_USER",
+  "DB_DATABASE",
+  "DB_PASSWORD",
+];
 
-const requiredVarNames = ['DB_PORT', 'DB_HOST', 'DB_USER', 'DB_DATABASE', 'DB_PASSWORD']
-
-for (const varNames of requiredVarNames) {
-   if(!process.env[varNames]) {
-    throw new Error(`Missing required fields: ${varNames}`)
-   }
-    
+for (const varName of requiredVarNames) {
+  if (!process.env[varName]) {
+    throw new Error(`Missing required field: ${varName}`);
+  }
 }
 
-
 const pool = new Pool({
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    max: 10,
-    idleTimeoutMillis: 3000,
-    connectionTimeoutMillis: 2000
-})
+  port: parseInt(process.env.DB_PORT, 10),
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  max: 10,
+  idleTimeoutMillis: 3000,
+  connectionTimeoutMillis: 2000,
+});
 
-export {pool};
+export { pool };
