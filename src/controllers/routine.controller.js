b02f61e8f890dd68b216a716d25085f54ff414ae
Muneeb381a <muneebafzal381a@gmail.com>
2025-05-26 16:09:01 +0500

    Refactor getAllRoutines to improve error handling and response structure

--- src/controllers/routine.controller.js
+++ src/controllers/routine.controller.js
@@ -1,22 +1,23 @@
 import { pool } from "../config/db.js";
 
-
-// get all the routines
-
 export const getAllRoutines = async (req, res) => {
-
-    try {
-        const {rows} = await pool.query('SELECT * FROM routine ORDER BY date ASC')
-        res.status(200).json({
-            success: true,
-            message: "Data retrived Sucesfully",
-            data: rows
-        })
-    } catch (err) {
-        console.error("Error while getting all the routines", err)
-        res.status(400).json({
-            success: false,
-            message: "Error while getting all the routines",
-        })
-    }
-}
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM routine ORDER BY date ASC"
+    );
+    res.status(200).json({
+      success: true,
+      message: "All routines fetched succesfully",
+      count: rows.length,
+      date: new Date().toISOString(),
+      data: rows,
+    });
+  } catch (err) {
+    console.error("Error occured while getting routines", err);
+    res.status(400).json({
+      success: false,
+      message: "Error occured while retriving data",
+      date: new Date().toISOString(),
+    });
+  }
+};
